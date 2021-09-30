0a603f29a01536059d39d9dce32cb0f8153e79e3
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2021-09-30 19:27:06 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BaCaplus
+# baca
+Untuk tutorial nya
+baca aja article nya bro  
+https://fullblack999.blogspot.com/2019/08/cara-nuyul-baca-plus-terbaru.html?m=1
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="2DDE29FC08C5957932C45F76D5EDB085";
+$raw_uid="27&sv=23.5.1&s";
+$user_name="subur martiana";
+$invite_code="F4qIVyNRiI";
+$user_token="92.0.4515.131";
+$own_mac="1632958489";
+$android_id="OPM1.171019.026";
+$device_type="Linux; Android 8.1.0; vivo 1811 Build/OPM1.171019.026; wv";
+
+
+?>
